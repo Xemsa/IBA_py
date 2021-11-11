c478dd49599e563a21e399503375c5b4f135b61a
Xemsa <Sektorianin@gmail.com>
2021-11-12 00:19:08 +0300

    11.11

--- 3/Xemsa_3_1.py
+++ 3/Xemsa_3_1.py
@@ -1,11 +1,17 @@
 #3.1
 #largest of 4
 
-def all2f(n):
-    if isinstance(n, int):
+def tr(n):
+    try:
+        return int(n)
+    except:
+        pass
+    try:
         return float(n)
-    else:
-        return n
+    except:
+        pass
+    return 0
+
 
 def maxof2(n1,n2):
     if n1 >= n2:
@@ -13,13 +19,19 @@ def maxof2(n1,n2):
     elif n2 > n1:
         return  n2
 
+a=b=c=d=0
+print('a=')
+a=tr(input())
 
-a=all2f(input())
-b=2
-c=3
-d=4
+print('b=')
+b=tr(input())
+
+print('c=')
+c=tr(input())
+
+print('d=')
+d=tr(input())
 
 m=maxof2(maxof2(a,b),maxof2(c,d))
+print('наибольшее число',m)
 
-
-print(m)
