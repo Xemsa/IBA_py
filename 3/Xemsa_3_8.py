ccd1179edd411d0f7a186a58532c83d9faa35fa3
Xemsa <Sektorianin@gmail.com>
2021-11-13 16:02:51 +0300

    11.13

---
+++ 3/Xemsa_3_8.py
@@ -0,0 +1,13 @@
+#3.8
+# Design a program that uses a while loop 
+# and continuously asks the user to enter 
+# a word unless the user enters 
+# "chupacabra" as the secret exit word, 
+# in which case the message "You've 
+# successfully left the loop." should be 
+# printed to the screen, and the loop 
+# should terminate.
+be=''
+while be!="chupacabra":
+    be=input()
+print("You've successfully left the loop.")
