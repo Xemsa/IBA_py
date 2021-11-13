e880e0153dcb39a82b415bef63dbe74745878cf4
Xemsa <Sektorianin@gmail.com>
2021-11-13 16:12:12 +0300

    11.13

--- 3/Xemsa_3_7.py
+++
@@ -1,9 +0,0 @@
-#3.7
-#sleep
-import time
-
-# for i in range (3):
-for i in ['one Mississippi', 'two Mississippi', 'three Mississippi']:
-    print(i)
-    time.sleep(1)
-print("Ready or not, here I come!")
