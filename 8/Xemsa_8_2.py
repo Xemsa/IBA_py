a2c68af0b3c15afb8cdb93f9982e76e864e652d4
Xemsa <Sektorianin@gmail.com>
2021-11-27 18:47:12 +0300

    11.27 7.1

---
+++ 8/Xemsa_8_2.py
@@ -0,0 +1,18 @@
+#8.1.1
+#8.2
+#???
+
+"""method, i.e.:
+• it should accept exactly one argument - a 
+string;
+• it should return a list of words created from 
+the string, divided in the places where the 
+string contains whitespaces;
+• if the string is empty, the function should 
+return an empty list;
+• its name should be mysplit()
+• Use the template in the editor. Test your 
+code carefully."""
+
+def mysplit(dtf:str):
+    
